0b39f462673f0b6f987087eb1bdf3878b5b78fc0
saya0714 <38k.xxxx.3838@gmail.com>
2021-06-23 18:49:07 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,67 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :edit, :destroy, :update]
+  before_action :move_to_index, only: [:edit, :update]
+  
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+    redirect_to root_path
+    else
+    render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+  end
+  
+  def edit
+    @prototype = Prototype.find(params[:id])
+    if @prototype.user_id == current_user.id
+      render :edit
+    else
+      redirect_to root_path 
+    end
+  end 
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+      end
+    end
+
+    def destroy
+      @prototype = Prototype.find(params[:id])    
+      @prototype.destroy
+      redirect_to root_path
+    end
+  
+
+  
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    prototype = Prototype.find(params[:id])
+    if prototype.user_id != current_user.id
+       redirect_to root_path                  #redirect_to root_path
+    end                                       #redirect_to root_path
+  end                                         #unless user_signed_in? == current_user.name                                      
+end
+ 
